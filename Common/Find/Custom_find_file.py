6c3ed7122a1e34592047719f15b61332a07260e5
Vahagn <lessons.qa@gmail.com>
2025-03-13 23:52:50 +0400

    Minor update. Fixed/added typle element types to not get warning

--- Common/Find/Custom_find_file.py
+++ Common/Find/Custom_find_file.py
@@ -6,8 +6,8 @@ class CustomFindClass:
     def __init__(self, driver):
         self.driver = driver
 
-    def custom_find_element(self, locator: tuple):
+    def custom_find_element(self, locator: tuple[str, str]):
         return WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(locator))
 
-    def custom_find_elements(self, locator: tuple):
+    def custom_find_elements(self, locator: tuple[str, str]):
         return WebDriverWait(self.driver, 10).until(ec.presence_of_all_elements_located(locator))
